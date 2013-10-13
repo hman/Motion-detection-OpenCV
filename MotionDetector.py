6b210c87a7e37e92ea3dbba0a5c9b68f921e9363
David Batranu <dbatranu@gmail.com>
2013-10-13 22:51:13 +0300

    Modifying code to work with kinect
    - using SimpleCV to grab Kinect depth cam
    - playing delightful Portal turret sounds when motion is detected

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,43 +1,69 @@
 import cv2.cv as cv
+import cv2
 from datetime import datetime
 import time
+import subprocess
+
+import SimpleCV
+import random
+
+
+sounds = [
+    "Turret_turret_autosearch_1.wav",
+    "Turret_turret_autosearch_2.wav",
+    "Turret_turret_autosearch_4.wav",
+    "Turret_turret_autosearch_5.wav",
+    "Turret_turret_autosearch_6.wav"
+]
+
+
+cam = SimpleCV.Kinect()
 
 class MotionDetectorInstantaneous():
-    
+
     def onChange(self, val): #callback when the user change the detection threshold
         self.threshold = val
-    
+
     def __init__(self,threshold=8, doRecord=True, showWindows=True):
         self.writer = None
         self.font = None
         self.doRecord=doRecord #Either or not record the moving object
         self.show = showWindows #Either or not show the 2 windows
         self.frame = None
-    
-        self.capture=cv.CaptureFromCAM(0)
-        self.frame = cv.QueryFrame(self.capture) #Take a frame to init recorder
+
+
+        #self.cam = cv2.VideoCapture(-1)
+        #self.cam.set(cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+        #self.cam.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 488)
+
+
+        #self.capture=cv.CaptureFromCAM(-1)
+        #self.frame = cv.QueryFrame(self.capture) #Take a frame to init recorder
+        #self.frame = cv.fromarray(self.cam.read()[1])
+        self.frame = cv.fromarray(cam.getDepth().getNumpyCv2())
         if doRecord:
             self.initRecorder()
-        
+
         self.frame1gray = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U) #Gray frame at t-1
         cv.CvtColor(self.frame, self.frame1gray, cv.CV_RGB2GRAY)
-        
+
         #Will hold the thresholded result
         self.res = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U)
-        
+
         self.frame2gray = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U) #Gray frame at t
-        
+
         self.width = self.frame.width
         self.height = self.frame.height
         self.nb_pixels = self.width * self.height
         self.threshold = threshold
         self.isRecording = False
         self.trigger_time = 0 #Hold timestamp of the last detection
-        
+        self.playing = False
+
         if showWindows:
             cv.NamedWindow("Image")
             cv.CreateTrackbar("Detection treshold: ", "Image", self.threshold, 100, self.onChange)
-        
+
     def initRecorder(self): #Create the recorder
         codec = cv.CV_FOURCC('M', 'J', 'P', 'G') #('W', 'M', 'V', '2')
         self.writer=cv.CreateVideoWriter(datetime.now().strftime("%b-%d_%H:%M:%S")+".wmv", codec, 5, cv.GetSize(self.frame), 1)
@@ -45,44 +71,57 @@ class MotionDetectorInstantaneous():
         self.font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 2, 8) #Creates a font
 
     def run(self):
-        started = time.time()
+        #started = time.time()
         while True:
-            
-            curframe = cv.QueryFrame(self.capture)
-            instant = time.time() #Get timestamp o the frame
-            
+
+            #curframe = cv.QueryFrame(self.capture)
+            #curframe = cv.fromarray(self.cam.read()[1])
+            curframe = cv.fromarray(cam.getDepth().getNumpyCv2())
+            #instant = time.time() #Get timestamp o the frame
+
             self.processImage(curframe) #Process the image
-            
-            if not self.isRecording:
-                if self.somethingHasMoved():
-                    self.trigger_time = instant #Update the trigger_time
-                    if instant > started +5:#Wait 5 second after the webcam start for luminosity adjusting etc..
-                        print datetime.now().strftime("%b %d, %H:%M:%S"), "Something is moving !"
-                        if self.doRecord: #set isRecording=True only if we record a video
-                            self.isRecording = True
+
+            if self.somethingHasMoved():
+                print 'MOTION!!!!'
+                if not self.playing:
+                    self.playing = True
+                    nr = random.randrange(len(sounds) - 1)
+                    subprocess.call(["aplay", sounds[nr]])
+                    self.playing = False
+                #cv.SaveImage('motion-%s.jpg' %  datetime.now().isoformat(), curframe)
             else:
-                if instant >= self.trigger_time +10: #Record during 10 seconds
-                    print datetime.now().strftime("%b %d, %H:%M:%S"), "Stop recording"
-                    self.isRecording = False
-                else:
-                    cv.PutText(curframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 0) #Put date on the frame
-                    cv.WriteFrame(self.writer, curframe) #Write the frame
-            
-            if self.show:
-                cv.ShowImage("Image", curframe)
-                cv.ShowImage("Res", self.res)
-                
+                print 'nothing...'
+
+            #if not self.isRecording:
+            #    if self.somethingHasMoved():
+            #        self.trigger_time = instant #Update the trigger_time
+            #        if instant > started +5:#Wait 5 second after the webcam start for luminosity adjusting etc..
+            #            print datetime.now().strftime("%b %d, %H:%M:%S"), "Something is moving !"
+            #            if self.doRecord: #set isRecording=True only if we record a video
+            #                self.isRecording = True
+            #else:
+            #    if instant >= self.trigger_time +10: #Record during 10 seconds
+            #        print datetime.now().strftime("%b %d, %H:%M:%S"), "Stop recording"
+            #        self.isRecording = False
+            #    else:
+            #        cv.PutText(curframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 0) #Put date on the frame
+            #        cv.WriteFrame(self.writer, curframe) #Write the frame
+
+            #if self.show:
+            #    cv.ShowImage("Image", curframe)
+            #    cv.ShowImage("Res", self.res)
+
             cv.Copy(self.frame2gray, self.frame1gray)
-            c=cv.WaitKey(1) % 0x100
-            if c==27 or c == 10: #Break if user enters 'Esc'.
-                break            
-    
+            #c=cv.WaitKey(1) % 0x100
+            #if c==27 or c == 10: #Break if user enters 'Esc'.
+            #    break
+
     def processImage(self, frame):
         cv.CvtColor(frame, self.frame2gray, cv.CV_RGB2GRAY)
-        
+
         #Absdiff to get the difference between to the frames
         cv.AbsDiff(self.frame1gray, self.frame2gray, self.res)
-        
+
         #Remove the noise and do the threshold
         cv.Smooth(self.res, self.res, cv.CV_BLUR, 5,5)
         cv.MorphologyEx(self.res, self.res, None, None, cv.CV_MOP_OPEN)
@@ -102,7 +141,7 @@ class MotionDetectorInstantaneous():
             return True
         else:
             return False
-        
+
 if __name__=="__main__":
-    detect = MotionDetectorInstantaneous(doRecord=True)
+    detect = MotionDetectorInstantaneous(doRecord=False, showWindows=False)
     detect.run()
